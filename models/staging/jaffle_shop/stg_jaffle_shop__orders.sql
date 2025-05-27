bf4c3845e708d612859011c749e57cddc9385d18
Rumana Patel <rumana142@gmail.com>
2025-05-27 13:05:34 +0000

    add stg_orders model with standardised column naming

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status,
+        current_timestamp() as _loaded_at
+
+    from source
+
+)
+
+select * from renamed
